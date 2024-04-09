3f606f69ae6705d1e1cc832ed8db2a3ded9656a2
Jeeho Ahn <jeeho@umich.edu>
2024-04-09 11:34:03 -0400

    added interpolation for generating a path

--- demos/testDubins.cpp
+++ demos/testDubins.cpp
@@ -9,6 +9,8 @@
 #include <ompl/geometric/planners/rrt/RRT.h>
 
 #include <cmath>
+#include <chrono>
+
 #define M_PI 3.14159265358979323846 /* pi */
 
 namespace ob = ompl::base;
@@ -46,10 +48,73 @@ struct State
     int time;
 };
 
-
-ompl::base::DubinsStateSpace::DubinsPath findDubins(State &start, State &goal)
+void jeeho_interpolate(const OmplState *from, const ompl::base::DubinsStateSpace::DubinsPath &path, double t,
+                       OmplState *state, ompl::base::DubinsStateSpace* space, double turning_radius)
 {
-    ompl::base::DubinsStateSpace dubinsSpace(1.0);
+    OmplState *s = space->allocState()->as<OmplState>();
+    double seg = t * path.length(), phi, v;
+
+    s->setXY(0,0);
+    s->setYaw(from->getYaw());
+    if (!path.reverse_)
+    {
+        for (unsigned int i = 0; i < 3 && seg > 0; ++i)
+        {
+            v = std::min(seg, path.length_[i]);
+            phi = s->getYaw();
+            seg -= v;
+            switch (path.type_[i])
+            {
+                case ompl::base::DubinsStateSpace::DUBINS_LEFT:
+                    s->setXY(s->getX() + sin(phi + v) - sin(phi), s->getY() - cos(phi + v) + cos(phi));
+                    s->setYaw(phi + v);
+                    break;
+                case ompl::base::DubinsStateSpace::DUBINS_RIGHT:
+                    s->setXY(s->getX() - sin(phi - v) + sin(phi), s->getY() + cos(phi - v) - cos(phi));
+                    s->setYaw(phi - v);
+                    break;
+                case ompl::base::DubinsStateSpace::DUBINS_STRAIGHT:
+                    s->setXY(s->getX() + v * cos(phi), s->getY() + v * sin(phi));
+                    break;
+            }
+        }
+    }
+    else
+    {
+        for (unsigned int i = 0; i < 3 && seg > 0; ++i)
+        {
+            v = std::min(seg, path.length_[2 - i]);
+            phi = s->getYaw();
+            seg -= v;
+            switch (path.type_[2 - i])
+            {
+                case ompl::base::DubinsStateSpace::DUBINS_LEFT:  // DUBINS_LEFT
+                    s->setXY(s->getX() + sin(phi - v) - sin(phi), s->getY() - cos(phi - v) + cos(phi));
+                    s->setYaw(phi - v);
+                    break;
+                case ompl::base::DubinsStateSpace::DUBINS_RIGHT:  // DUBINS_RIGHT
+                    s->setXY(s->getX() - sin(phi + v) + sin(phi), s->getY() + cos(phi + v) - cos(phi));
+                    s->setYaw(phi + v);
+                    break;
+                case ompl::base::DubinsStateSpace::DUBINS_STRAIGHT:  // DUBINS_STRAIGHT
+                    s->setXY(s->getX() - v * cos(phi), s->getY() - v * sin(phi));
+                    break;
+            }
+        }
+    }
+
+
+    state->setX(s->getX() * turning_radius + from->getX());
+    state->setY(s->getY() * turning_radius + from->getY());
+    space->getSubspace(1)->enforceBounds(s->as<OmplState>(1));
+    state->setYaw(s->getYaw());
+    space->freeState(s);
+}
+
+
+ompl::base::DubinsStateSpace::DubinsPath findDubins(State &start, State &goal, double turning_radius = 1.0)
+{
+    ompl::base::DubinsStateSpace dubinsSpace(turning_radius);
     OmplState *dubinsStart = (OmplState *)dubinsSpace.allocState();
     OmplState *dubinsEnd = (OmplState *)dubinsSpace.allocState();
     dubinsStart->setXY(start.x, start.y);
@@ -57,6 +122,8 @@ ompl::base::DubinsStateSpace::DubinsPath findDubins(State &start, State &goal)
     dubinsEnd->setXY(goal.x, goal.y);
     dubinsEnd->setYaw(-goal.yaw);
     ompl::base::DubinsStateSpace::DubinsPath dubinsPath = dubinsSpace.dubins(dubinsStart, dubinsEnd);
+    dubinsStart->setXY(start.x, start.y);
+    dubinsStart->setYaw(-start.yaw);
 
     for (auto pathidx = 0; pathidx < 3; pathidx++)
     {
@@ -80,7 +147,22 @@ ompl::base::DubinsStateSpace::DubinsPath findDubins(State &start, State &goal)
         std::cout << fabs(dubinsPath.length_[pathidx]) << std::endl;
     }
 
+    OmplState *interState = (OmplState *)dubinsSpace.allocState();
     // auto path_g = generateSmoothPath(dubinsPath,0.1);
+
+    size_t num_pts = 100;
+
+    for (size_t n=0; n<num_pts; n++)
+    {
+        //auto start = std::chrono::steady_clock::now();
+        jeeho_interpolate(dubinsStart, dubinsPath, (double)n / (double)num_pts, interState, &dubinsSpace,
+                          turning_radius);
+        //auto end = std::chrono::steady_clock::now();
+        //auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+        //std::cout << "Elapsed time: " << duration << " usec" << std::endl;
+
+        std::cout << interState->getX() << " " << interState->getY() << " " << interState->getYaw() << std::endl;
+    }
 
     return dubinsPath;
 }
